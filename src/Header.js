024a698a4e302d520cc77f385a9a9e48e3b813a9
Hongbin Li <hblee12294@gmail.com>
2018-11-17 15:48:25 -0800

    feat: added Header

---
+++ src/Header.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import './Header.css'
+import logo from './images/logo2.png'
+
+class Header extends Component {
+
+  render() {
+    return (
+      <header>
+        <div className="header-wrap">
+          <a href="#" className="logo">
+            <img id="header-logo" src={ logo } alt="logo-white"/>
+          </a>
+          <ul className="menu">
+            <li className="menu-item"><a href="#">Home</a></li>
+            <li className="menu-item"><a href="#">About</a></li>
+            <li className="menu-item"><a href="#">News</a></li>
+            <li className="menu-item"><a href="#">Campus</a></li>
+            <li className="menu-item"><a href="#">Contact</a></li>
+            <li className="menu-item"><a href="#">Career</a></li>
+          </ul>
+        </div>
+      </header>
+    )
+  }
+}
+
+export default Header
