d4f5f389dbaaf3ada656649bbf15f967c6ae5076
Hongbin Li <hblee12294@gmail.com>
2018-11-19 16:35:07 -0800

    Updates

---
+++ precache-manifest.c30cc14a8fc58ea825afb2ff8eee8069.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b85afe0adc731481e18c",
+    "url": "/rdsr/static/css/main.97b6dc37.chunk.css"
+  },
+  {
+    "revision": "b85afe0adc731481e18c",
+    "url": "/rdsr/static/js/main.b85afe0a.chunk.js"
+  },
+  {
+    "revision": "852e8df8770f0b281dad",
+    "url": "/rdsr/static/js/1.852e8df8.chunk.js"
+  },
+  {
+    "revision": "6bbe41e424763625cfec",
+    "url": "/rdsr/static/js/runtime~main.6bbe41e4.js"
+  },
+  {
+    "revision": "92e6c236d53ff61d9edf2168688c9411",
+    "url": "/rdsr/static/media/logo2.92e6c236.png"
+  },
+  {
+    "revision": "c83c2807c7286d7dd4f88675609dbbc3",
+    "url": "/rdsr/static/media/5a45a3ceea8ac.c83c2807.jpg"
+  },
+  {
+    "revision": "419e34e724b60e1dabded008a918c35b",
+    "url": "/rdsr/static/media/5a45a3f215f2e.419e34e7.jpg"
+  },
+  {
+    "revision": "2be332a526560e70c0ea8c35ba977172",
+    "url": "/rdsr/static/media/5a45a3fcc0a49.2be332a5.jpg"
+  },
+  {
+    "revision": "5ec12e041ecdaf492aaf47c2e731ac35",
+    "url": "/rdsr/static/media/5a56ccaacf1e1.5ec12e04.jpg"
+  },
+  {
+    "revision": "72bab68e4d66e197f720d46f96c8d01e",
+    "url": "/rdsr/index.html"
+  }
+];
